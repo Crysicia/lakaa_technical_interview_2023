255e58ec3fffcb385174b96b7e332deed18557e0
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-05-31 01:35:39 +0200

    Fix front formatting

--- lakaa_frontend/src/views/Store.vue
+++ lakaa_frontend/src/views/Store.vue
@@ -1,40 +1,40 @@
 
 <template>
-	<h1 class="is-center">Store</h1>
+  <h1 class="is-center">Store</h1>
 
-	<div class="card">
-		<header><h4>Create collection</h4></header>
- 		<FormError :errors="errorMessage" />
- 		<div class="row">
-			<div class="col">
-  			<label for="organization_name">Organization name</label>
-  			<input id="organization_name" v-model="newCollection.organization_name" placeholder="Organization name" />
-			</div>
-			<div class="col">
-  			<label for="collected_at">Collection date</label>
-				<input id="collected_at" v-model="newCollection.collected_at" type='date' placeholder="Collection date" />
- 			</div>
-		</div>
+  <div class="card">
+    <header><h4>Create collection</h4></header>
+     <FormError :errors="errorMessage" />
+     <div class="row">
+      <div class="col">
+        <label for="organization_name">Organization name</label>
+        <input id="organization_name" v-model="newCollection.organization_name" placeholder="Organization name" />
+      </div>
+      <div class="col">
+        <label for="collected_at">Collection date</label>
+        <input id="collected_at" v-model="newCollection.collected_at" type='date' placeholder="Collection date" />
+       </div>
+    </div>
 
-  		<h4>Indicators</h4>
-  		<div class="container">
-	  		<div class="row" v-for="indicator in newCollection.indicators">
-	  			<div class="col">
-		  			{{ indicator.name }}
-	  			</div>
-	  			<div class="col-3">
-	  				<input v-model="indicator.value" placeholder="Indicator value" />
-		  		</div>
-		  		<div class="col">
-		  			{{ indicator.unit }}
-	  			</div>
-	  		</div>
-	  	</div>
-  		<footer class="is-right">
-  			<button class="button primary" @click="create">New collection</button>
-  		</footer>
-	</div>
-	<PastCollections :collections="collections" />
+      <h4>Indicators</h4>
+      <div class="container">
+        <div class="row" v-for="indicator in newCollection.indicators">
+          <div class="col">
+            {{ indicator.name }}
+          </div>
+          <div class="col-3">
+            <input v-model="indicator.value" placeholder="Indicator value" />
+          </div>
+          <div class="col">
+            {{ indicator.unit }}
+          </div>
+        </div>
+      </div>
+      <footer class="is-right">
+        <button class="button primary" @click="create">New collection</button>
+      </footer>
+  </div>
+  <PastCollections :collections="collections" />
 </template>
 
 <script>
@@ -44,7 +44,7 @@
   import FormError from "../components/FormError.vue";
 
   export default {
-  	components: { PastCollections, FormError },
+    components: { PastCollections, FormError },
     data() {
       return {
         newCollection: {
@@ -57,32 +57,32 @@
       }
     },
     async mounted() {
-		  this.collections = await CollectionService.findAll();
-		  this.setIndicators();
-		},
+      this.collections = await CollectionService.findAll();
+      this.setIndicators();
+    },
     methods: {
       reset() {
-				this.newCollection = {
-				  organization_name: '',
-				  collected_at: '',
-				  indicators: [],
-				};
-				this.errorMessage = ''
-				this.setIndicators();
-		  },
+        this.newCollection = {
+          organization_name: '',
+          collected_at: '',
+          indicators: [],
+        };
+        this.errorMessage = ''
+        this.setIndicators();
+      },
 
       async create() {
-      	try {
-	        const res = await CollectionService.create(this.newCollection);
-	        this.collections.push(res.data);
-	        this.reset();
-      	} catch (error) {
-      		this.errorMessage = error.response.data;
-      	}
+        try {
+          const res = await CollectionService.create(this.newCollection);
+          this.collections.push(res.data);
+          this.reset();
+        } catch (error) {
+          this.errorMessage = error.response.data;
+        }
       },
 
       async setIndicators() {
-      	this.newCollection.indicators = await IndicatorService.findAllRequired();
+        this.newCollection.indicators = await IndicatorService.findAllRequired();
       },
     },
   };
