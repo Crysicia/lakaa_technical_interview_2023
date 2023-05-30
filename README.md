89ec5fe580452f26511e4ba94c09b2bfe85cb88d
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-05-30 22:36:25 +0200

    Remove style.css and unused Axios configuration, add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lakaa technical interview (Full-stack internship)
+
+## How to run the app 🔧
+**Backend**
+- Go to the backend folder: `cd lakaa_backend`
+- Craete the database and run the migrations `rails db:create db:migrate`
+- Run the server `rails s`
+
+**Frontend**
+- Go to the frontend folder: `cd lakaa_frontend`
+- Install the dependencies `npm install`
+- Run the server `npm start dev`
--- lakaa_frontend/src/main.js
+++ lakaa_frontend/src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
-// import './style.css'
 import 'chota'
 
-import { securedAxiosInstance, plainAxiosInstance } from './services/axios'
 import router from './services/router'
-import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-createApp(App).use(router).use(VueAxios, {
-  secured: securedAxiosInstance,
-  plain: plainAxiosInstance
-}).mount('#app')
+createApp(App).use(router).mount('#app')
